a2bb0a885c18a985de3bb340c6fcfa021fa8b974
Tanguy Vaessen <seza443@users.noreply.github.com>
2019-07-10 14:55:22 +0200

    Create nerf-countdown.js
    initial script

---
+++ nerf-countdown.js
@@ -0,0 +1,72 @@
+// Description:
+//   ▄︻̷̿┻̿═━一 When life at the office is sad, grab a nerf gun and start a war ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot nerf - Hubot initiate a Nerf Battle Countdown
+//   hubot shoot - Hubot initiate a Nerf Battle Countdown
+//   hubot gun - Hubot initiate a Nerf Battle Countdown
+//   hubot war - Hubot initiate a Nerf Battle Countdown
+//
+// Author:
+//   seza443
+//
+// Sources:
+//  https://github.com/seza443/hubot-nerf-countdown
+
+const INITIAL_COUNTER = 5;
+const TIMER = 1000;
+
+// https://textfac.es/
+const GUNS = [
+  "▄︻̷̿┻̿═━一",
+  "̿̿ ̿̿ ̿̿ ̿'̿'\\̵͇̿̿\\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿",
+  "̿'̿'\\̵͇̿̿\\з=( ͠° ͟ʖ ͡°)=ε/̵͇̿̿/'̿̿ ̿ ̿ ̿ ̿ ̿",
+  "̿ ̿ ̿'̿'\\̵͇̿̿\\з=(•_•)=ε/̵͇̿̿/'̿'̿ ̿",
+  "⌐╦╦═─",
+  "━╤デ╦︻(▀̿̿Ĺ̯̿̿▀̿ ̿)",
+
+];
+
+const FIGHT_MSG = [
+  "SHOOT",
+  "FIGHT",
+  "TAKE COVER",
+  "RUN FOR YOUR LIFE",
+  "GRENAAAAAADE",
+  "ENNEMY INCOMING",
+  "POINT & CLICK",
+];
+
+/**
+ * Send a message with the counter value, then recursive call with counter--
+ * When counter is 0, send a GUNS message
+ *
+ * @param robot
+ * @param msg
+ * @param counter
+ */
+function nerfCountdown(robot, msg, counter) {
+  if (counter > 0) {
+    msg.send('' + counter);
+    setTimeout(
+      function() {
+        nerfCountdown(robot, msg, counter - 1);
+      }, TIMER);
+  } else {
+    msg.send(msg.random(FIGHT_MSG));
+    msg.send(msg.random(GUNS));
+  }
+}
+
+module.exports = function (robot) {
+
+  robot.respond(/(nerf)|(gun)|(war)|(shoot)$/i, function (msg) {
+    nerfCountdown(robot, msg, INITIAL_COUNTER);
+  });
+};
